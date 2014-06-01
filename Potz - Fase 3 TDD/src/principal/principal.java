86d5fc019dbb89493171ce91ca41c941dda29ff5
Lorrayne Patricia Pereira <lorraynepatricia@outlook.com>
2014-06-01 15:59:43 -0300

    Refatoração

--- Potz - Fase 3 TDD/src/principal/principal.java
+++ Potz - Fase 3 TDD/src/principal/principal.java
@@ -2,33 +2,32 @@ package principal;
 import java.util.Scanner;
 
 import controler.controlador;
-
 public class principal {
 
-	private static Scanner ler;
+	private static Scanner leitura;
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-
 		//Varivel que recebe o cupom
 		String Cupom = "";
-		String Msg   = "";
-		ler = new Scanner(System.in);
+		String informacao;
+		leitura = new Scanner(System.in);
 		System.out.println("Informe o número do cupom POTZ:");
-		Cupom = ler.next();
-
+		Cupom = leitura.next();
 		controlador Control = new controlador();
-
+		//Chama método validacupom e passa o cupom fornecido pelo usuário para validar se o cupom está correto
 		if (Control.validaCupom(Cupom)){
+			
 			if (Control.valida_codigo_cupom(Cupom)){
 				int pontos	= Integer.parseInt(Cupom.substring(0,3));
-				Msg = "O cupom POTZ inserido é valido. O total de pontos adquiridos é: "+Integer.toString(pontos);	
+				System.out.println(informacao = "O cupom POTZ inserido é valido. O total de pontos adquiridos é: "+ Integer.toString(pontos));	
 			} else {
-				Msg = "O cupom POTZ inserido é inválido!";	
+				System.out.println(informacao = "O cupom POTZ inserido é inválido!");	
 			}
-		}else{
-			Msg = "O cupom POTZ inserido é inválido!";	
+		}
+		
+		else{
+			System.out.println(informacao = "O cupom POTZ inserido é inválido!");	
 		}
 
-		System.out.println(Msg);
 	}
 }
